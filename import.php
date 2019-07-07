4ec61b169d1921de424b5ff93f71b82e62acc2c2
LevAyv <LevonAyvazyan43@gmail.com>
2019-07-07 22:00:32 +0400

    conditions flow done , mysql carcas only , gitignore  private.php

--- import.php
+++ import.php
@@ -11,17 +11,16 @@ foreach ($valid_args as &$item){
     $item = "-" . $item;
 }
 //var_dump($valid_args);
-//die("YOU SHALL NOT PASS");
+//dieD("YOU SHALL NOT PASS");
 /** check if arguments count is valid , argv[0] is this php file name */
 if ($argc < 2 || $argc > 5){
-  die("Temp: Invalid argument count: $argc \n");
+    dieD("Temp: Invalid argument count: $argc \n");
 }
 // check if argument values are valid
 // check 1st argument - EXPORT or IMPORT
-echo "asdasd ".$argv[1]." asdasd\n";
 if (!in_array($argv[1], $valid_args)) {
     $string = implode(" , ", $valid_args);
-    die("Temp: Invalid first argument: $argv[1] (can be one of $string ) \n");
+    dieD("Temp: Invalid first argument: $argv[1] (can be one of $string ) \n");
     //todo parse $valid_args array to single string
 } else { //todo remove else clause
     echo "good 1st arg \n";
@@ -33,33 +32,52 @@ $myArgs['db_name'] = $argv[2];
 
 // detect missing arguments and set default values
 if ($argc-1 == 2) {
-    echo "case 2 args";
+//    echo "case 2 args \n";
+    dieD ("Missing functionality - 2 argument not supported YET ");
 }
 if ($argc-1 == 3) {
-    echo "case 3 args";
+//    echo "case 3 args \n";
     if (validateFileArg($argv[3])){
         $myArgs['filename'] = $argv[3];
     }
+    dieD ("Missing functionality - 3 argument not supported YET ");
 }
 if ($argc-1 == 4) {
-    echo "case 4 args";
-    if (validateFileArg($argv[3])){
-        $myArgs['filename'] = $argv[3];
+//    echo "case 4 args \n";
+    if (validateDbTableArg($argv[3])){
+        $myArgs['table_names'] = $argv[3];
+    }
+    if (validateFileArg($argv[4])){
+        $myArgs['file_name'] = $argv[4];
     }
 }
+doStuff($myArgs);
+
 function validateDbTableArg($string){
-    if (preg_match('/[^A-Za-z0-9$_]/', $string)) {
-        die("Temp: Invalid characters in DB/Table name : $string (can be one only A-Z , a-z , 0-9 , $ , _ ) \n");
+    if (preg_match('/[^A-Za-z0-9$_,]/', $string)) {
+        dieD("Temp: Invalid characters in DB/Table name : $string (can be one only A-Z , a-z , 0-9 , $ , _ ) \n");
     }
     return true;
 }
 function validateFileArg($string){
     if (preg_match('/[^A-Za-z0-9._]/', $string)) {
-        die("Temp: Invalid characters in Table name : $string (can be one only A-Z , a-z , 0-9 , . , _ ) \n");
+        dieD("Temp: Invalid characters in Table name : $string (can be one only A-Z , a-z , 0-9 , . , _ ) \n");
     }
     return true;
 }
-
+function dieD($string){
+    die("\n\n Error: ".$string."\n");
+}
+function doStuff($args){ // TODO export to separate file ?
+    echo " start doing staff \n";
+    mysqli_connect(
+        "localhost",
+        "",
+        "",
+        "",
+        ""
+    );
+}
 /*
  $count = 0;
 foreach ($valid_args as $item){
