f83bf7e6dceacfd5ec6532a154682d8ce468a404
LevAyv <LevonAyvazyan43@gmail.com>
2019-07-15 00:08:07 +0400

    exporting data too

--- bin/sql_parser.php
+++ bin/sql_parser.php
@@ -2,15 +2,54 @@
 
 require_once 'lib/logger.php';
 $connection = null;
+$data_too = true;
 
 /**
  * @param $args
  */
 function export($args){
+    global $data_too;
+    export_structure($args);
+    if ($data_too){
+        export_data($args);
+    }
+}
+/*
+INSERT INTO customers VALUES
+('126', ... , '21000.00');
+
+
+
+
+*/
+function export_data($args){
     $table_names = $args['table_names'];
-    $sql_skeleton = "SHOW CREATE TABLE ";
+    // todo make function instead of SQL skeletons
     if (!is_array($table_names)){
-        //case 1 single table names
+        //case table_names is string , cast it to array
+        $array_buffer[0] = $table_names;
+        $table_names = $array_buffer;
+    }
+    $data = get_all_rows_data($table_names[0]);
+    $i= 0;
+    foreach ($table_names as $value_Table) {
+        foreach ($data as $value_Data) {
+            $array_to_write[$i++] = build_sql_insert_into($value_Table , $value_Data);
+        }
+    }
+    $delimiter = "\n";
+    if (!isset($array_to_write)) { //todo remove ?
+        dieSafely("BUG function export_data() array_to_write is null");
+    }
+    $string_to_write = implode($delimiter, $array_to_write);
+    file_write_append($args['file_name'] , $string_to_write);
+
+
+}
+function export_structure($args){
+    $table_names = $args['table_names'];
+    if (!is_array($table_names)){
+        //case table_names is string , cast it to array
         $array_buffer[0] = $table_names;
         $table_names = $array_buffer;
     }
@@ -20,7 +59,7 @@ function export($args){
 //    }
 //    // todo overkill --- end --- ???
     foreach ($table_names as $key => $value) {
-        $sql_array[$key] = $sql_skeleton.$value.";";
+        $sql_array[$key] = build_sql_show_create_table($value);
     }
     $iter = 0;
     /** @var array $sql_array each row represents table structure recreation query*/
@@ -32,18 +71,63 @@ function export($args){
         }
     }
     $delimiter = "; \n\n### END ###\n\n";
-    /** @var array $table_structure */
     $string_to_write = implode($delimiter, $table_structure);
-    // todo file path validation
-    $file_name = $args['file_name'];
-    $file = fopen("$file_name",'w');
-    if (!fwrite($file,"$string_to_write"))
-        dieSafely("file error");
-    fclose($file);
+    $string_to_write.=$delimiter;
 
+    file_truncate($args['file_name']);
+    file_write_append($args['file_name'] , $string_to_write);
     log4("Export completed successfully");
 }
-
+function file_truncate($file_name){
+    $file = fopen("$file_name",'a');
+    fclose($file);
+}
+function file_write_append($file_name , $data ){
+    $file = fopen("$file_name",'a');
+    if (!fwrite($file,"$data"))
+        dieSafely("file error");
+    fclose($file);
+}
+function build_sql_show_create_table($table_name){
+    $sql_skeleton = "SHOW CREATE TABLE ";
+    return $sql_skeleton.$table_name.";";
+}
+function build_sql_insert_into($table_name , $data){
+    $sql_skeleton_1 = "INSERT INTO ";
+    $sql_skeleton_2 = " VALUES (";
+    $sql_skeleton_3 = ");";
+    foreach ($data as &$item){
+        if ($item == ""){
+            $item = "NULL";
+        }else{
+            $item = "'".$item."'";
+        }
+    }
+    $data = implode(",",$data);
+    return $sql_skeleton_1.$table_name.$sql_skeleton_2.$data.$sql_skeleton_3;
+}
+function get_db_names(){
+    $sql = "SHOW DATABASES;";
+    $db_names = query_get_db_or_table_names($sql,null);
+    return $db_names;
+}
+function get_table_names($db){
+    $sql1 = "USE ".$db.";";
+    $sql2 = "SHOW TABLES;";
+    $table_names = query_get_db_or_table_names( $sql1 , $sql2);
+    return $table_names;
+}
+function get_all_rows_data($table){
+    $sql_skeleton = "SELECT * FROM ";
+    $sql_select = $sql_skeleton.$table.";";
+    $result = query_get_something($sql_select);
+    $iter = 0;
+    while ($row = mysqli_fetch_assoc($result)){
+        $data[$iter] = $row;
+        $iter++;
+    }
+    return $data;
+}
 /**
  * @param $sql
  * @return mysqli_result|bool
@@ -60,19 +144,6 @@ function query_get_something($sql){
     }
     return $result;
 }
-
-function get_db_names(){
-    $sql = "SHOW DATABASES;";
-    $db_names = query_get_db_or_table_names($sql,null);
-    return $db_names;
-}
-function get_table_names($db){
-    $sql1 = "USE ".$db.";";
-    $sql2 = "SHOW TABLES;"; //todo bug asap
-    $table_names = query_get_db_or_table_names( $sql1 , $sql2);
-    return $table_names;
-}
-
 /**
  * @param $sql_db string for "USE database_name;" query
  * @param $sql_t string for "SHOW CREATE TABLE table_name;" query
@@ -88,7 +159,7 @@ function query_get_db_or_table_names($sql_db, $sql_t){
         $result  = query_get_something($sql_t);
     }
     $iter=0;
-    $names = null; //todo useless ? research
+//    $names = null; //todo useless ? research
     while ($row = mysqli_fetch_array($result)){
         $names[$iter] = "$row[0]";
         $iter++;
--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 require_once 'config/credentials.php';
 require_once 'bin/validator.php';
 require_once 'bin/sql_parser.php';
-require_once 'lib/logger.php';
+require_once 'bin/lib/logger.php';
 
 logger_on();
 log4("Entry point");
